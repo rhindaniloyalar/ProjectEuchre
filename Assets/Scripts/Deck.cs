498a556a3bbb9ca6e2b8f2e982f65b4ea96eeb91
Christine Jordan <rhindani@live.com>
2017-05-03 10:00:30 -0400

    Base code for decks and cards

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,40 +1,70 @@
 ﻿/****************************Deck.cs********************************
-*Purpose: Houses the deck of cards and all funtions pertaining to
 *Programmers: Christine Jordan
+*Purpose: Houses the deck of cards and all funtions pertaining to
+*Inherits: 
 *Date Created: [4/24/17]
 *Date Modified: [4/24/17] 
 *********************************************************************/
-using System;
 using System.Collections.Generic;
-{
-    
-}
+using UnityEngine;
+
+
 public class Deck
 {
-    private enum suit
-    {
-        heart,
-        club,
-        spade,
-        dimond;
-    }
-
-    private enum rank
-    {
-        2, 3, 4, 5, 6,
-        7, 8, 9, 10,
-        j, q, k, a;
-    }
-
-    stack 
+    protected const int NUM_CARDS = 52;
+    protected Stack<Card> deck = new Stack<Card>();
+    //can't shuffle a stack, so we need a list.
+    protected List<Card> cardList = new List<Card>();
 	
     /****************************Deck()*****************************
     *In:
     *Out:
     *Description
     ****************************************************************/
-    Deck ()
+    public Deck ()
     {
+        Card newCard;
+       
+        for (int i = 0; i < NUM_CARDS; ++i)
+        {
+            newCard = new Card(i);
+            cardList.Add(newCard);
+        }
 
+        Shuffle(cardList);
+    }
+
+
+    /****************************Deck()*****************************
+    *In:
+    *Out:
+    *Description
+    ****************************************************************/
+    protected void Shuffle(List<Card> l)
+    {
+        for (int i = 0; i < l.Count; ++i)
+        {
+            int rand = Random.Range(i, l.Count);
+            Card temp = l[rand];
+            l[rand] = l[i];
+            l[i] = temp;
+        }
+        
+        //we'll push the deck onto the stack from the last
+        //element of the array to the first so that the first
+        //card delt is that of the 0th array index
+        for(int i = l.Count-1; i <=0; --i)
+        {
+            deck.Push(l[i]);
+        }
+    }
+
+    public void DebugDeck()
+    {
+        for (int i = 0; i < deck.Count; ++i)
+        {
+            Card card = deck.Pop();
+            Debug.Log(card);
+        }
     }
 }
---
+++ Assets/Scripts/EuchreDeck.cs
@@ -0,0 +1,43 @@
+﻿/*************************EuchreDeck*********************************
+*Programmers: Christine Jordan 
+*Purpose: Holds all info for the euchre decks
+*Inherits: Deck.cs
+*Date Created: [4/25/17]
+*Date Modified: [4/25/17]
+*********************************************************************/
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuchreDeck : Deck
+{
+    //The minimum rank that a card needs to be for Euchre.
+    const int EUCHRE_RANK = 7;
+
+    /************************EuchreDeck()****************************
+    *In:
+    *Out:
+    *Description: Takes the traditional 52 card deck and makes a 
+    *             Euchre deck from it by putting just the 9, 10, J,
+    *             K, Q, A into the card list to be shuffled.
+    ****************************************************************/
+    public EuchreDeck() : base()
+    {
+
+        List<Card> temp = new List<Card>();
+
+        temp.AddRange(cardList);
+        cardList.Clear();
+        deck.Clear();
+        //pull the cards 9, 10 J, K, Q, A into a new list.
+        foreach (Card c in temp)
+        {
+            if ((int)c.GetRank() >= EUCHRE_RANK)
+            {
+                cardList.Add(c);
+            }
+        }
+
+        Shuffle(cardList);
+    }
+}
